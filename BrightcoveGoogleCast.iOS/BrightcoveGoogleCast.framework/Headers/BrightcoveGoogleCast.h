b82026860c3a51c13b2d0cb66b32c1dc51c8f1c2
Sweekriti Satpathy <swsat@microsoft.com>
2020-03-02 18:09:00 -0800

    Exoplayer and intial brightcove binding

--- BrightcoveGoogleCast.iOS/BrightcoveGoogleCast.framework/Headers/BrightcoveGoogleCast.h
+++
@@ -1,9 +0,0 @@
-//
-// BrightcoveGoogleCast.h
-// BrightcoveGoogleCast
-//
-// Copyright (c) 2019 Brightcove, Inc. All rights reserved.
-// License: https://accounts.brightcove.com/en/terms-and-conditions
-//
-
-#import <BCOVGoogleCastManager.h>
