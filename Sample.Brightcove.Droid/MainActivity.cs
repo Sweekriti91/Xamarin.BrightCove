e5d7f9a1ef608a07c8fc7e69b1e60f34bd9b0c7d
Sweekriti Satpathy <swsat@microsoft.com>
2020-04-06 16:22:25 -0700

    makes playback values variables

--- Sample.Brightcove.Droid/MainActivity.cs
+++ Sample.Brightcove.Droid/MainActivity.cs
@@ -15,6 +15,10 @@ namespace Sample.Brightcove.Droid
     {
         static BrightcoveVideoView brightcoveVideoView;
 
+        static string policyKEY = "BCpkADawqM3zXLtsEM0nAyA_3o3TmZnG6bZTXFmjZ8X_rmFMqlpB78l0aiRELs7MWACf4mYN92qMOLMxfZN6Xr3cQ_0R3G2qBiho3X3Nc2yTv7DH4APQ-EimMJQ3crX0zc0mJMy9CtSqkmli";
+        static string accountID = "3303963094001";
+        string videoId = "4283173439001";
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -23,9 +27,9 @@ namespace Sample.Brightcove.Droid
             SetContentView(Resource.Layout.activity_main);
 
             brightcoveVideoView = FindViewById<BrightcoveVideoView>(Resource.Id.brightcove_video_view);
-            Catalog catalog = new Catalog(brightcoveVideoView.EventEmitter, "3303963094001", "BCpkADawqM3zXLtsEM0nAyA_3o3TmZnG6bZTXFmjZ8X_rmFMqlpB78l0aiRELs7MWACf4mYN92qMOLMxfZN6Xr3cQ_0R3G2qBiho3X3Nc2yTv7DH4APQ-EimMJQ3crX0zc0mJMy9CtSqkmli");
+            Catalog catalog = new Catalog(brightcoveVideoView.EventEmitter, accountID, policyKEY);
 
-            catalog.FindVideoByID(videoID: "4283173439001", new VideoListenerR());
+            catalog.FindVideoByID(videoID: videoId, new VideoListenerR());
         }
 
         public partial class VideoListenerR : VideoListener
