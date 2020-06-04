75289d6ebe8d80a6a8e201c17d81d884f733dc28
Sweekriti Satpathy <swsat@microsoft.com>
2020-06-04 15:00:21 -0700

    updates to cast droid

--- Sample.Brightcove.Droid/MainActivity.cs
+++ Sample.Brightcove.Droid/MainActivity.cs
@@ -10,21 +10,30 @@ using Com.Brightcove.Player.Events;
 using AndroidX.Fragment.App;
 using Android.Gms.Cast.Framework.Media.Widget;
 using AndroidX.AppCompat.App;
+using Android.Gms.Cast;
+using Xamarin.Essentials;
+using Com.Brightcove.Cast.Util;
+using System.Linq;
+using System.Collections.Generic;
+using Android.Media;
+using MediaInfo = Android.Gms.Cast.MediaInfo;
+using System;
 
 namespace Sample.Brightcove.Droid
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-        static BrightcoveVideoView brightcoveVideoView;
-
+        static BrightcoveExoPlayerVideoView brightcoveVideoView;
+        //Brightcove DRM Asset
         //static string policyKEY = "BCpkADawqM3zXLtsEM0nAyA_3o3TmZnG6bZTXFmjZ8X_rmFMqlpB78l0aiRELs7MWACf4mYN92qMOLMxfZN6Xr3cQ_0R3G2qBiho3X3Nc2yTv7DH4APQ-EimMJQ3crX0zc0mJMy9CtSqkmli";
         //static string accountID = "3303963094001";
         //string videoId = "4283173439001";
 
-        static string policyKEY = "BCpkADawqM1W-vUOMe6RSA3pA6Vw-VWUNn5rL0lzQabvrI63-VjS93gVUugDlmBpHIxP16X8TSe5LSKM415UHeMBmxl7pqcwVY_AZ4yKFwIpZPvXE34TpXEYYcmulxJQAOvHbv2dpfq-S_cm";
-        static string accountID = "3636334163001";
-        string videoId = "3637773814001";
+        //Non-DRM Asset
+        //static string policyKEY = "BCpkADawqM1W-vUOMe6RSA3pA6Vw-VWUNn5rL0lzQabvrI63-VjS93gVUugDlmBpHIxP16X8TSe5LSKM415UHeMBmxl7pqcwVY_AZ4yKFwIpZPvXE34TpXEYYcmulxJQAOvHbv2dpfq-S_cm";
+        //static string accountID = "3636334163001";
+        //string videoId = "3637773814001";
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -38,6 +47,7 @@ namespace Sample.Brightcove.Droid
             Catalog catalog = new Catalog(eventEmitter, accountID, policyKEY);
 
             catalog.FindVideoByID(videoID: videoId, new VideoListenerR());
+
 
             //GoogleCastComponent googleCastComponent = new GoogleCastComponent(eventEmitter, this);
             //googleCastComponent.IsSessionAvailable;
@@ -73,19 +83,19 @@ namespace Sample.Brightcove.Droid
                 Source source = findCastableSource(video);
 
                 GoogleCastComponent googleCastComponent = new GoogleCastComponent(eventEmitter, Platform.AppContext);
-                //MediaInfo mediaInfo = CastMediaUtil.toMediaInfo(video, source, null, null);
-                //googleCastComponent.loadMediaInfo(mediaInfo);
-
+                MediaInfo mediaInfo = CastMediaUtil.ToMediaInfo(video, source, null, null);
+                googleCastComponent.LoadMediaInfo(mediaInfo);
 
                 //You can check if there is a session available
                 //googleCastComponent.isSessionAvailable();
             }
         }
+
         public static Source findCastableSource(Video video)
         {
             Source savedDashSource = null;
 
-            if (video.SourceCollections.Count == 0
+            if (video.SourceCollections.Count != 0
                     && video.SourceCollections.ContainsKey(DeliveryType.Dash)
                     && video.SourceCollections.Values.Where(c => c.DeliveryType == DeliveryType.Dash) != null) 
             {
@@ -93,8 +103,9 @@ namespace Sample.Brightcove.Droid
                 List<Source> dashSource = new List<Source>();
                 foreach(var d in dashSourceCollections)
                 {
-                    var item = d.Sources.Where(a => a.DeliveryType == DeliveryType.Dash).FirstOrDefault();
-                    dashSource.Add(item);
+                    //var item = d.Sources.Where(a => a.Url.Contains("dash")).ToList<Source>();
+                    var item = d.Sources.Where(a => a.DeliveryType == DeliveryType.Dash).ToList<Source>();
+                    dashSource = item;
                 }
                 
                 foreach (var src in dashSource)
@@ -103,14 +114,16 @@ namespace Sample.Brightcove.Droid
 
                     if (src.Url.Contains("ac-3_avc1_ec-3_mp4a"))
                     {
+                        Console.WriteLine("SOURCE :: " + src);
                         return src;
                     }
                 }
+
+                Console.WriteLine("SOURCE :: " + savedDashSource);
                 return savedDashSource;
             }
             return null;
         }
-
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
