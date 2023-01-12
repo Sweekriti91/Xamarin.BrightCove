b57c98051a343f2496267e4784bf5244f7c6a806
Sweekriti Satpathy <swsat@microsoft.com>
2023-01-11 17:57:51 -0800

    sample builds,but runtime error

--- Sample.Brightcove.Droid/MainActivity.cs
+++ Sample.Brightcove.Droid/MainActivity.cs
@@ -23,7 +23,7 @@ using System;
 namespace Sample.Brightcove.Droid
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : BrightcovePlayer
     {
         static BrightcoveExoPlayerVideoView brightcoveVideoView;
         //Brightcove DRM Asset
